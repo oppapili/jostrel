0fa59e14170db1087b213c9ff41b7d67c8a68964
oppapili <pitipitiunsyumikan@gmail.com>
2025-05-24 15:44:35 +0000

    chore: Initialize project

---
+++ src/main/java/io/github/oppapili/jostrel/JostrelApplication.java
@@ -0,0 +1,13 @@
+package io.github.oppapili.jostrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JostrelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JostrelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/oppapili/jostrel/JostrelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.oppapili.jostrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JostrelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
